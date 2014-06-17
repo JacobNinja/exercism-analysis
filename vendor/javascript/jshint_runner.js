96bb951aa97ffbf9ee2e42c82dc88ce301999d95
Jacob Richardson <jacob.ninja.dev@gmail.com>
2014-06-17 11:30:15 -0600

    Add encoding to node readFileSync so it returns a string

--- vendor/javascript/jshint_runner.js
+++ vendor/javascript/jshint_runner.js
@@ -1,6 +1,6 @@
 var jshint = require('jshint').JSHINT;
 var fs = require('fs');
-var code = fs.readFileSync(process.argv[2]);
+var code = fs.readFileSync(process.argv[2], {encoding: 'utf8'});
 
 jshint(code);
 console.log(JSON.stringify(jshint.data().errors));
