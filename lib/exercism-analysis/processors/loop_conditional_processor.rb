1c21fc0e98e2fc3f4079ded7eba2d30216b8af80
Jacob Richardson <jacob.ninja.dev@gmail.com>
2015-03-29 16:09:53 -0600

    Add all Enumerable methods that take a block to LoopConditionalProcessor

--- lib/exercism-analysis/processors/loop_conditional_processor.rb
+++ lib/exercism-analysis/processors/loop_conditional_processor.rb
@@ -3,7 +3,13 @@ class Exercism
 
     class LoopConditionalProcessor < Processor
 
-      ENUMERABLE_METHODS = Set.new(%w(each))
+      ENUMERABLE_METHODS = %w(
+        all? any? chunk collect collect_concat count cycle detect drop_while each
+        each_cons each_entry each_slice each_with_index each_with_object
+        find find_all find_index flat_map grep group_by inject map max max_by
+        min min_by minmax minmax_by none? one? partition reduce reject reverse_each
+        select slice_after slice_before slice_when sort sort_by take_while zip
+      ).to_set
 
       def initialize
         @conditionals = []
